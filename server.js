4612823c03f44b5570e6d241be26ac1ccc1d88b5
zmdhillo <zmdhillo@gmail.com>
2015-12-04 18:50:43 +0500

    Node.js Integration.

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ db = new neo4j('http://neo4j:123@localhost:7474');
 var express = require('express');
 var bodyParser = require('body-parser');
 
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.set('/client', __dirname + '../../client');
+app.engine('html', require('ejs').renderFile);
+
+require('./util')(app, neo4j, db);
+
+app.use(express.static('client'));
+
+
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function () {
     var listeningPort = server.address().port;
--- util/util.js
+++ util/util.js
@@ -2,6 +2,40 @@ module.exports = function (app, neo4j, db) {
     app.get('/', function (req, res) {
         res.render('../client/index.html');
     });
+    app.get('/home', function (req, res) {
 
+        loop().then(function () {
+            res.send(arr);
+            console.log(arr);
+
+        });
+    });
+    app.post('/add', function (req, res) {
+        console.log(req.body.item);
+        db.insertNode(req.body, function (err, node) {
+
+            if (err) res.json(err);
+
+            // Output node properties.
+            console.log(node.data);
+
+            // Output node id.
+            console.log(node._id);
+            res.json(node);
+        });
+
+    });
+    var arr = [];
+    function loop() {
+        for (var i = 191; i < 210; i++) {
+            db.readNode(i, function (err, node) {
+                arr.push(node);
+            });
+        }
+    }
 
 };
+
+
+
+
